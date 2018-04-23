5b6dc727a0cdcd0dd6e3f07cc47fe29ccdf6cc3a
James Bell <jbell303@gmail.com>
2018-04-23 12:45:48 -0700

    add multiple squadrons

--- index.js
+++ index.js
@@ -7,103 +7,107 @@ var moment = require('moment');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-var squadron = 'vt-7';
+var squadrons = ['vt-7', 'vt-9'];
 const uri = 'https://www.cnatra.navy.mil/scheds/schedule_data.aspx';
-const date = moment().startOf('day').add(1, 'days').format('DDMMMYYYY');
-
-// .NET postback parameters
-var options =  {
-  method: 'GET',
-  uri: uri,
-  resolveWithFullResponse: true,
-  qs: {
-    sq: squadron,
-  },
-  formData: {
-    __EVENTTARGET: '',
-    __EVENTARGUMENT: '',
-    __VIEWSTATE : '',
-    __VIEWSTATEGENERATOR: '',
-    __EVENTVALIDATION: '',
-    txtNameSearch: '',
-  },
-  headers: {
-    'User-Agent': 'Request-Promise'
-  },
-  transform: function (body) {
-    return cheerio.load(body);
-  }
-}
-
-// s3 paramaters
-var s3 = new AWS.S3();
-var path = squadron + "/" + date + ".xml";
-var params = {
-  Bucket : "iflypod-xml-repository",
-  Key : path
-}
+// const date = moment().startOf('day').add(1, 'days').format('DDMMMYYYY');
+const date = moment().startOf('day').format('DDMMMYYYY');
+var options = '';
 
 exports.handler = (event, context, callback) => {
-  // check if object exists
-  s3.headObject(params, function (err, metadata) {
-    if (err && err.code === 'NotFound') {
-      // retrieve schedule and write to XML
-      console.log('fetching schedule...');
-      rp (options)
-        .then(($) => {
-          // console.log("First call: " + $.statusCode);
-          options.formData.__EVENTTARGET = 'ctrlCalendar';
-          options.formData.__EVENTARGUMENT = skedDateFormat(date);
-          saveViewstate($);
-          options.method = 'POST';
-
-          // send another postback
-          rp(options)
-            .then(($) => {
-              // console.log("Second call: " + $.statusCode);
-              options.formData.__EVENTTARGET = '';
-              options.formData.__EVENTARGUMENT = '';
-              saveViewstate($);
-
-              // retrieve the schedule
-              options.formData.btnViewSched = "View Schedule";
-              rp(options)
-                .then(($) => {
-                  // [DEBUG]
-                  // console.log($('#dgCoversheet').text());
-                  // console.log($('#dgEvents').text());
-                  // callback(null, "success");
-
-                  // console.log("Third call: " + $.statusCode);
-                  //parse html to XML
-                  var xml_parser = new XMLParser($, date, squadron);
-                  params.Body = xml_parser.parse();
-
-                  // write to S3 bucket
-                  console.log('schedule fetched, writing to s3: ' + path);
-                  s3.putObject(params, function(err, data) {
-                    if (err) callback(err);
-                    else {
-                      callback(null, path + " written successfully! " + data);
-                    }
-                  });
-                })
-                .catch((err) => {
-                  callback(err);
-                })
-            })
-            .catch((err) => {
-              callback(err);
-            })
-        })
-        .catch((err) => {
-          callback(err);
-        })
-    } else {
-      // object already exists
-      callback(null, "object: " + path + " already exists! " + moment().format('MMMM Do YYYY, h:mm:ss a'));
+  squadrons.forEach(function(squadron) {
+    // .NET postback parameters
+    options =  {
+      method: 'GET',
+      uri: uri,
+      resolveWithFullResponse: true,
+      qs: {
+        sq: squadron,
+      },
+      formData: {
+        __EVENTTARGET: '',
+        __EVENTARGUMENT: '',
+        __VIEWSTATE : '',
+        __VIEWSTATEGENERATOR: '',
+        __EVENTVALIDATION: '',
+        txtNameSearch: '',
+      },
+      headers: {
+        'User-Agent': 'Request-Promise'
+      },
+      transform: function (body) {
+        return cheerio.load(body);
+      }
     }
+    // s3 paramaters
+    var s3 = new AWS.S3();
+    var path = squadron + "/" + date + ".xml";
+    var params = {
+      Bucket : "iflypod-xml-repository",
+      Key : path
+    }
+    // check if object exists
+    s3.headObject(params, function (err, metadata) {
+      if (err && err.code === 'NotFound') {
+        // retrieve schedule and write to XML
+        console.log('fetching schedule...');
+        rp (options)
+          .then(($) => {
+            // console.log("First call: " + $.statusCode);
+            options.formData.__EVENTTARGET = 'ctrlCalendar';
+            options.formData.__EVENTARGUMENT = skedDateFormat(date);
+            saveViewstate($);
+            options.method = 'POST';
+
+            // send another postback
+            rp(options)
+              .then(($) => {
+                // console.log("Second call: " + $.statusCode);
+                options.formData.__EVENTTARGET = '';
+                options.formData.__EVENTARGUMENT = '';
+                saveViewstate($);
+
+                // retrieve the schedule
+                options.formData.btnViewSched = "View Schedule";
+                rp(options)
+                  .then(($) => {
+                    // [DEBUG]
+                    // console.log($('#dgCoversheet').text());
+                    // console.log($('#dgEvents').text());
+                    // callback(null, "success");
+
+                    // console.log("Third call: " + $.statusCode);
+                    //parse html to XML
+                    var xml_parser = new XMLParser($, date, squadron);
+                    params.Body = xml_parser.parse();
+
+                    // write to S3 bucket
+                    console.log('schedule fetched, writing to s3: ' + path);
+                    s3.putObject(params, function(err, data) {
+                      if (err) console.log(err);
+                      else {
+                        // add schedule to available list
+                        console.log(path + " written successfully! " + data);
+                      }
+                    });
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                  })
+              })
+              .catch((err) => {
+                console.log(err);
+              })
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+      } else {
+        // object already exists
+        console.log("object: " + path + " already exists! " + moment().format('MMMM Do YYYY, h:mm:ss a'));
+      }
+    })
   })
+  callback(null, "success!");
 }
 
   function saveViewstate($) {
